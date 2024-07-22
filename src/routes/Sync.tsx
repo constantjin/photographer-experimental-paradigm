68a545f0d8737d4812060f245ee8e350c0871570
Sangsoo, Jin <swingwithblue@gmail.com>
2024-07-22 16:50:28 +0900

    refactor: Fix useEffect lint warning in Sync.tsx
    If a function is not re-used, move it inside the useEffect hook

--- src/routes/Sync.tsx
+++ src/routes/Sync.tsx
@@ -9,48 +9,34 @@ import { channels } from "@constants";
 export function Sync() {
   const divRef = useRef<HTMLDivElement>(null);
   const navigate = useNavigate();
-  const dataDirPaths = useAtomValue(dataDirPathsAtom);
-
-  // const handleSyncInput = async (
-  //   event: React.KeyboardEvent<HTMLDivElement>
-  // ) => {
-  //   if (event.code === "KeyS") {
-  //     const etimeResponse = await window.api.invoke(
-  //       "write-etime",
-  //       dataDirPaths.participantRunDataDirPath,
-  //       "sync"
-  //     );
-  //     reportAPIResponse(etimeResponse);
-  //     navigate("/instruction");
-  //   }
-  // };
-
-  const handleSyncInputInDocument = async (event: KeyboardEvent) => {
-    if (event.code === "KeyS") {
-      const etimeResponse = await window.api.invoke(
-        channels.WRITE_ETIME,
-        dataDirPaths.participantRunDataDirPath,
-        "sync",
-      );
-      reportAPIResponse(etimeResponse);
-      navigate("/instruction");
-    }
-  };
+  const participantRunDataDirPath =
+    useAtomValue(dataDirPathsAtom).participantRunDataDirPath;
 
   useEffect(() => {
+    const handleSyncInputInDocument = async (event: KeyboardEvent) => {
+      if (event.code === "KeyS") {
+        const etimeResponse = await window.api.invoke(
+          channels.WRITE_ETIME,
+          participantRunDataDirPath,
+          "sync",
+        );
+        reportAPIResponse(etimeResponse);
+        navigate("/instruction");
+      }
+    };
+
     divRef.current?.focus();
     document.addEventListener("keydown", handleSyncInputInDocument);
 
     return () => {
       document.removeEventListener("keydown", handleSyncInputInDocument);
     };
-  }, []);
+  }, [navigate, participantRunDataDirPath]);
 
   return (
     <div
       className="flex items-center text-white max-w-lg border-none focus:ring-0 outline-none focus:outline-none"
       tabIndex={0}
-      // onKeyDown={handleSyncInput}
       ref={divRef}
     >
       <p className="font-bold text-3xl">Waiting for sync...</p>
