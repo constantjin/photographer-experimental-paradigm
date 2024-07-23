394239ce7a320200fff91b8e82621d1c0afeefaa
Sangsoo, Jin <swingwithblue@gmail.com>
2024-07-23 12:07:35 +0900

    refactor: Update FixationTarget.tsx
    - Join promises using Promise.all()
    - Remove useCallback() and move predictTextFeature in useEffect()
    - Use page-specific constants from experimentalSettingAtom

--- src/routes/FixationTarget.tsx
+++ src/routes/FixationTarget.tsx
@@ -1,64 +1,70 @@
-import { useEffect, useCallback } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAtomValue, useSetAtom } from "jotai";
 
 import { delay, jitter } from "@/utils";
 import { reportAPIResponse } from "@/utils/api";
-import { dataDirPathsAtom, currentRunInfoAtom } from "@/stores/experiment";
+import {
+  experimentalSettingAtom,
+  dataDirPathsAtom,
+  currentRunInfoAtom,
+} from "@/stores/experiment";
 import { clipTextFeatureAtom } from "@/stores/clip";
 import { channels } from "@constants";
 
 export function FixationTarget() {
-  // Page-specific constants
-  const fixationTargetDurationInMs = 5000; // ms
-  const jitterRatio = 0.2;
-
+  const experimentalSetting = useAtomValue(experimentalSettingAtom);
   const dataDirPaths = useAtomValue(dataDirPathsAtom);
   const currentRunInfo = useAtomValue(currentRunInfoAtom);
   const setClipTextFeature = useSetAtom(clipTextFeatureAtom);
 
+  const fixationTargetDurationInMs =
+    experimentalSetting?.trialInfo.fixationDurationInMs ?? 5000; // default value for fixation duration;
+  const jitterRatio = experimentalSetting?.trialInfo.fixationJitterRatio ?? 0.2; // default value for jitter ratio
+
   const navigate = useNavigate();
 
-  const predictTextFeature = useCallback(async () => {
-    const jitteredDurationInMs = jitter(
-      fixationTargetDurationInMs,
-      jitterRatio,
-    );
-    const startTime = performance.now();
+  useEffect(() => {
+    const predictTextFeature = async () => {
+      const jitteredDurationInMs = jitter(
+        fixationTargetDurationInMs,
+        jitterRatio,
+      );
 
-    const etimeResponse = await window.api.invoke(
-      channels.WRITE_ETIME,
-      dataDirPaths.participantRunDataDirPath,
-      "fixation_target",
-    );
-    reportAPIResponse(etimeResponse);
+      await Promise.all([
+        window.api
+          .invoke(
+            channels.WRITE_ETIME,
+            dataDirPaths.participantRunDataDirPath,
+            "fixation_target",
+          )
+          .then((etimeResponse) => reportAPIResponse(etimeResponse)),
+        window.api
+          .invoke(
+            channels.CLIP.PREDICT_CLIP_TEXT,
+            currentRunInfo?.captionTarget,
+            dataDirPaths.runFeatureVectorDirPath,
+            "text_feature",
+          )
+          .then((clipTextResponse) => {
+            setClipTextFeature(clipTextResponse.data);
+            reportAPIResponse(clipTextResponse);
+          }),
+        delay(jitteredDurationInMs),
+      ]);
 
-    const clipTextResponse = await window.api.invoke(
-      channels.CLIP.PREDICT_CLIP_TEXT,
-      currentRunInfo?.captionTarget,
-      dataDirPaths.runFeatureVectorDirPath,
-      "text_feature",
-    );
-    reportAPIResponse(clipTextResponse);
-    setClipTextFeature(clipTextResponse.data);
-    const endTime = performance.now();
-
-    const totalDelay = endTime - startTime;
-    if (totalDelay < jitteredDurationInMs) {
-      await delay(fixationTargetDurationInMs - totalDelay);
-    }
-    navigate("/exploration");
+      navigate("/exploration");
+    };
+    predictTextFeature();
   }, [
     currentRunInfo?.captionTarget,
     dataDirPaths.participantRunDataDirPath,
     dataDirPaths.runFeatureVectorDirPath,
+    fixationTargetDurationInMs,
+    jitterRatio,
     navigate,
     setClipTextFeature,
   ]);
-
-  useEffect(() => {
-    predictTextFeature();
-  }, [predictTextFeature]);
 
   return (
     <div className="flex items-center text-white max-w-lg">
