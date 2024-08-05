f197b01480d6f73d7b5c9b680f2a06738c500cb0
Sangsoo, Jin <swingwithblue@gmail.com>
2024-08-05 16:22:35 +0900

    chore: Mention license for codes from josephrocca/openai-clip-js, clip-bpe-js

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -8,7 +9,29 @@ export function jitter(baseInMs: number, ratio: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Reference: https://github.com/josephrocca/openai-clip-js/commits/main/onnx-image-demo.html
+// normalizedImageBuffer() is from https://github.com/josephrocca/openai-clip-js/commits/main/onnx-image-demo.html
+// MIT License
+
+// Copyright (c) 2021 josephrocca
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 export async function noramlizeImageBuffer(inputBuffer: Uint8Array) {
   const blobFromBuffer = new Blob([inputBuffer], { type: "image/png" });
   const img = await createImageBitmap(blobFromBuffer);
