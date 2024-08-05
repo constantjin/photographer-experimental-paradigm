13115c3c266fff4e3e8f5a6218de1295cdcd2f44
Sangsoo, Jin <swingwithblue@gmail.com>
2024-08-05 16:00:27 +0900

    refactor: Remove unnecessary stores in streetview.ts and capture.ts

--- src/stores/capture.ts
+++ src/stores/capture.ts
@@ -1,13 +1,7 @@
 import { atom } from "jotai";
 
-export const captureTimerEnabledAtom = atom(false);
-
-export const capturedVisibleAtom = atom(false);
-
-// New
-
 export const captureIntervalEnableAtom = atom(false);
 export const capturePageLoadAtom = atom(false);
-export const enableControllerActionAtom = atom(false);
+
 export const base64EncodedCaptureAtom = atom("");
 export const base64EncodedVoiceAtom = atom("");
